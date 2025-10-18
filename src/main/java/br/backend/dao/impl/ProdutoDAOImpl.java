04b78acff7d24df739266f104c83a25887272299
LorenzoBruscato <lorenzobruscato6@gmail.com>
2025-10-18 16:42:12 -0300

    fix: classes dos produtos

--- src/main/java/br/backend/dao/impl/ProdutoDAOImpl.java
+++ src/main/java/br/backend/dao/impl/ProdutoDAOImpl.java
@@ -2,6 +2,15 @@ package br.backend.dao.impl;
 
 import br.backend.dao.ProdutoDAO;
 import br.backend.database.Database;
+import br.backend.modelo.Categoria;
+import br.backend.modelo.Produto;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ProdutoDAOImpl implements ProdutoDAO {
 
@@ -11,5 +20,41 @@ public class ProdutoDAOImpl implements ProdutoDAO {
         this.database = database;
     }
 
+    @Override
+    public void inserirProduto(Produto obj) {
+        String sql = "INSERT into Produto (nome, preco_unitario, unidade, quantidade_minima_ quantidade maxima, categoria)" + "VALUES (?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement st = database.getConnection().prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            st.setString(1, obj.getNome());
+            st.setDouble(2, obj.getPreco());
+            st.setString(3, obj.getUnidade());
+            st.setInt(4, obj.getQuantidadeMinima());
+            st.setInt(5, obj.getQuantidadeMaxima());
+            st.setString(6, obj.getCategoria().getNome());
+            st.executeUpdate();
+
+            try (ResultSet rs = st.getGeneratedKeys()) {
+                if (rs.next()) {
+                    obj.setId(rs.getInt(1));
+                }
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdutoDAOImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public void atualizarProduto(Produto obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Produto buscarPorId(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public List<Produto> resgatarProdutos() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
 
 }
